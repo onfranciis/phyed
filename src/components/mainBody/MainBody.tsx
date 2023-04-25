7c5dbbf0ed3b73b97e863972c0dd7f35d4fc723e
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-04-25 08:12:36 +0100

    feat: create MainBody

---
+++ src/components/mainBody/MainBody.tsx
@@ -0,0 +1,12 @@
+import "../../styles/App.scss";
+import Dashboard from "./dashboard/Dashboard";
+
+const MainBody = () => {
+  return (
+    <div className="MainBody">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default MainBody;
